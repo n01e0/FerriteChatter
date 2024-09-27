9253acd3bcfe5561ddaae45476c25bf67d39c57f
n01e0 <reoshiseki@gmail.com>
2024-09-28 01:42:09 +0900

    add secret to ci

--- src/core.rs
+++ src/core.rs
@@ -1,11 +1,10 @@
 use anyhow::{anyhow, Result};
 use clap::ValueEnum;
+use model_gen::generate_models;
 use serde::de::{self, Deserializer, Visitor};
 use serde::Deserialize;
 use std::convert::TryFrom;
 use std::fmt;
-use model_gen::generate_models;
-
 
 generate_models!();
 pub const DEFAULT_MODEL: Model = Model::Gpt_4o;
